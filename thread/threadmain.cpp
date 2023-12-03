6e151601d368a8a6e7182c4efde2584595be45c0
HikkizZ <fmiranda.reb@gmail.com>
2023-12-03 22:19:12 +0000

    Threads V2, correcto funcionamiento, Falta Readme

--- thread/threadmain.cpp
+++ thread/threadmain.cpp
@@ -1,62 +1,58 @@
 #include <iostream>
 #include <opencv2/opencv.hpp>
 #include <thread>
-#include <vector>
-#include <chrono>
 
 using namespace cv;
 using namespace std;
 
+// Definir una función para convertir una parte de la imagen a escala de grises
 void convertToGrayscale(Mat& image, int startRow, int endRow) {
-    for (int r = startRow; r < endRow; ++r) {
-        for (int c = 0; c < image.cols; ++c) {
+    for (int r = startRow; r < endRow; r++) {
+        for (int c = 0; c < image.cols; c++) {
             Point3_<uchar>* p = image.ptr<Point3_<uchar>>(r, c);
-            // Convierte a escala de grises utilizando el método de luminosidad
-            uchar grayValue = 0.299 * p->x + 0.587 * p->y + 0.114 * p->z;
-            p->x = p->y = p->z = grayValue;
+            uchar gray = static_cast<uchar>((p->x + p->y + p->z) / 3);
+            p->x = p->y = p->z = gray;
         }
     }
 }
 
 int main(int argc, char** argv) {
     if (argc != 4) {
-        cerr << "Uso: " << argv[0] << " <imagen_color> <imagen_gris> <num_hebras>" << endl;
+        cout << "Usage: " << argv[0] << " <imagen_a_color> <imagen_grises> <num_hebras>" << endl;
         return -1;
     }
 
     Mat image = imread(argv[1], IMREAD_COLOR);
+
     if (image.empty()) {
-        cerr << "Error al leer la imagen de entrada." << endl;
+        cout << "No es posible leer la imagen." << endl;
         return -1;
     }
 
     int numThreads = stoi(argv[3]);
+    int rowsPerThread = image.rows / numThreads;
     vector<thread> threads;
 
-    int rowsPerThread = image.rows / numThreads;
-    int remainingRows = image.rows % numThreads;
-
-    auto start_time = chrono::high_resolution_clock::now();
-
-    // Lanzar threads
-    for (int i = 0; i < numThreads; ++i) {
+    auto inicio = chrono::high_resolution_clock::now();
+    // Crear threads
+    for (int i = 0; i < numThreads; i++) {
         int startRow = i * rowsPerThread;
-        int endRow = (i == numThreads - 1) ? startRow + rowsPerThread + remainingRows : startRow + rowsPerThread;
+        int endRow = (i == numThreads - 1) ? image.rows : (i + 1) * rowsPerThread;
         threads.emplace_back(convertToGrayscale, ref(image), startRow, endRow);
     }
 
-    // Unir threads
     for (auto& t : threads) {
         t.join();
     }
 
-    auto end_time = chrono::high_resolution_clock::now();
-    chrono::duration<double> elapsed_seconds = end_time - start_time;
+    auto fin = chrono::high_resolution_clock::now();
+    auto duracion = chrono::duration_cast<chrono::milliseconds>(fin - inicio);
 
-    // Guardar la imagen en escala de grises
+    cout << "La conversion con thread fue completada en " << duracion.count() << " milisegundos" << endl;
+
+    // Guardar imagen en escala de grises
     imwrite(argv[2], image);
-
-    cout << "Total time spent in seconds is " << elapsed_seconds.count() << endl;
 
     return 0;
 }
+
